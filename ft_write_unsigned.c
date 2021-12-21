aa9c6fd17554995965228eac6470d842021da042
Yihyun Choi <yichoi@c8r2s11.42seoul.kr>
2021-12-21 16:25:43 +0900

    you could i dead

--- ft_write_unsigned.c
+++ ft_write_unsigned.c
@@ -6,19 +6,17 @@
 /*   By: yichoi <yichoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 19:32:05 by yichoi            #+#    #+#             */
-/*   Updated: 2021/12/16 21:00:47 by yichoi           ###   ########.fr       */
+/*   Updated: 2021/12/21 16:18:38 by yichoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "ft_printf.h"
 
-static int	nu_count(int n)
+static int	nu_count(unsigned int n)
 {
 	int	count;
 
 	count = 0;
-	if (n == 0)
-		count++;
 	while (n)
 	{
 		n /= 10;
@@ -32,14 +30,14 @@ static int	print_num(unsigned int n, int count)
 	char	*unsigned_string;
 	int		index;
 
-	unsigned_string = (char *)malloc(sizeof(char) * count + 1);
+	unsigned_string = (char *)malloc(sizeof(char) * (count + 1));
 	if (!unsigned_string)
 		return (0);
 	unsigned_string[count] = '\0';
 	index = count;
 	while (n)
 	{
-		unsigned_string[--index] = n % 10;
+		unsigned_string[--index] = n % 10 + '0';
 		n /= 10;
 	}
 	write(1, unsigned_string, count);
@@ -54,6 +52,8 @@ int	ft_write_unsigned(va_list ap)
 	int					len;
 
 	unsg_int = va_arg(ap, unsigned int);
+	if (unsg_int == 0)
+		return (write(1, "0", 1));
 	count = nu_count(unsg_int);
 	len = print_num(unsg_int, count);
 	return (len);
